4db3d39073b6b262cb0a85199fe6841f6a11c626
Amy <amykaletabe@gmail.com>
2022-06-01 11:36:27 +0200

    added hosting code

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ app.set("view engine", "ejs");
 
 //Show views
 app.get("/", function (req, res) {
-  res.render("index");
+  res.render("index.ejs");
 });
 
 app.get("/github", function (req, res) {
-  res.render("github");
+  res.render("github.ejs");
 });
 
 app.get("/heroku", function (req, res) {
-  res.render("heroku");
+  res.render("heroku.ejs");
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact");
+  res.render("contact.ejs");
 });
 
 //app listen on ports
-app.listen(process.envPORT || 1000, function () {
+app.listen(process.env.PORT || 1000, function () {
   console.log("website is running");
 });
